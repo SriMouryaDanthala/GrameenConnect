8dfadc2af36c1270336aecbd558ee607a49a8a1e
SriMouryaDanthala <danthalasrimourya@gmail.com>
2022-06-17 11:04:54 +0530

    1st commit in git

---
+++ app/src/main/java/com/example/grameenconnect/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.grameenconnect;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Intent i = new Intent(this,SplashScreen.class);
+        startActivity(i);
+
+    }
+}
